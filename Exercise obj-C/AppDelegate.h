c98a910af7e1fc28c691f76cf3894c07a0b17740
Benzs Jean Francois <benzsjeanfrancois@benzss-macbook-air.local>
2017-01-09 10:44:29 -0500

    Initial Commit

---
+++ Exercise obj-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Exercise obj-C
+//
+//  Created by Benzs Jean Francois on 1/9/17.
+//  Copyright © 2017 benzsjeanfrancois. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Exercise obj-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Exercise obj-C
+//
+//  Created by Benzs Jean Francois on 1/9/17.
+//  Copyright © 2017 benzsjeanfrancois. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
